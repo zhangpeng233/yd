25f105e88fbbcb45ab6917f4b9f3296a76accabd
zhangpeng233 <2241220338@qq.com>
2020-03-27 10:20:23 +0800

    yd

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+//Route::get('/', function () {
+//    return view('Index/Index');
+//});
+
+//Route::get('/register','Index\IndexController@Index');
